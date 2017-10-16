d869d10e4d7f81ed1b6843cf24df8469357d4400
fl4shk <fl4shk@users.noreply.github.com>
2017-10-16 09:01:54 -0400

    TestRam

--- src/top_level.sv
+++ src/top_level.sv
@@ -1,6 +1,48 @@
 `include "src/misc_defines.svinc"
 
 
+`define TEST_RAM_ADDR_WIDTH 32
+`define TEST_RAM_ADDR_MSB_POS `WIDTH_TO_MSB_POS(`TEST_RAM_ADDR_WIDTH)
+
+`define TEST_RAM_REAL_ADDR_WIDTH 8
+`define TEST_RAM_REAL_ADDR_MSB_POS \
+	`WIDTH_TO_MSB_POS(`TEST_RAM_REAL_ADDR_WIDTH)
+
+
+`define TEST_RAM_DATA_BUS_WIDTH 48
+`define TEST_RAM_DATA_BUS_MSB_POS \
+	`WIDTH_TO_MSB_POS(`TEST_RAM_DATA_BUS_WIDTH)
+
+
+// For testing instruction decoding
+module TestRam(input bit clk, enable,
+	input bit [`TEST_RAM_ADDR_MSB_POS:0] addr_in,
+	output bit [`TEST_RAM_DATA_BUS_MSB_POS:0] data_out);
+
+
+	bit [7:0] __mem[0:`TEST_RAM_REAL_ADDR_MSB_POS];
+
+	initial $readmemh(__mem, "readmemh_input.txt.ignore");
+
+	always @ (posedge clk)
+	begin
+		if (enable)
+		begin
+			data_out <= {__mem[addr_in & 8'hff],
+				__mem[(addr_in + 1) & 8'hff],
+
+				__mem[(addr_in + 2) & 8'hff],
+				__mem[(addr_in + 3) & 8'hff],
+
+				__mem[(addr_in + 4) & 8'hff],
+				__mem[(addr_in + 5) & 8'hff]};
+		end
+	end
+
+
+endmodule
+
+
 // Simulation top level module
 module TopLevel;
 
@@ -31,24 +73,6 @@ module TopLevel;
 		__half_clk = !__half_clk;
 	end
 
-
-
-	//always @ (posedge __master_clk)
-	//begin
-	//	//$display("__master_clk has ticked!");
-
-	//	//case (__half_clk)
-	//	//	1'b0:
-	//	//	begin
-	//	//		$display("__half_clk is low!");
-	//	//	end
-
-	//	//	1'b1:
-	//	//	begin
-	//	//		$display("__half_clk is high!");
-	//	//	end
-	//	//endcase
-	//end
 
 
 endmodule
